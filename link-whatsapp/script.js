a1deee2176c562a9928f520aac72b03efa1a9bca
VasKito43 <130059430+VasKito43@users.noreply.github.com>
2024-05-02 20:17:17 -0300

    02/05

--- link-whatsapp/script.js
+++ link-whatsapp/script.js
@@ -1,28 +1,44 @@
 var inputTexto = document.querySelector("#input")
 var divLink = document.querySelector("#link")
 var telefone = ""
+var txt = document.querySelector("#txt")
 
 
 inputTexto.addEventListener("input", atualizaTelefone)
 divLink.addEventListener("click", copiaLink)
 
+document.addEventListener("keydown", (evento) => {
+    let teclaPressionada = evento.key;
+
+    if (teclaPressionada == "Enter"){
+        geraLink()
+    }
+
+})
+
+
 function atualizaTelefone(){
-    telefone =  inputTexto.value.replace(/\D/g, "")
-    console.log(telefone)
-    if (telefone.length == 0) {
-        inputTexto.value  = ""
-    }else if (telefone.length == 1){
-        inputTexto.value = "(" + telefone
-    }else if (telefone.length < 8 && telefone.length >= 3){
-        inputTexto.value = "(" + telefone.slice(0, 2) + ")" + `${telefone.slice(2, 8)}`
-    } else if (telefone.length >= 8 && telefone.length < 12){
-        inputTexto.value = "(" + telefone.slice(0, 2) + ")" + `${telefone.slice(2, 7)}` + "-" + `${telefone.slice(7, 11)}`
-    } 
+    if (inputTexto.value.length <= 14){
+        telefone =  inputTexto.value.replace(/\D/g, "")
+        console.log(telefone)
+        if (telefone.length == 0) {
+            inputTexto.value  = ""
+        }else if (telefone.length == 1){
+            inputTexto.value = "(" + telefone
+        }else if (telefone.length < 8 && telefone.length >= 3){
+            inputTexto.value = "(" + telefone.slice(0, 2) + ")" + `${telefone.slice(2, 8)}`
+        } else if (telefone.length >= 8 && telefone.length < 12){
+            inputTexto.value = "(" + telefone.slice(0, 2) + ")" + `${telefone.slice(2, 7)}` + "-" + `${telefone.slice(7, 11)}`
+        } 
+    } else{
+        inputTexto.value = inputTexto.value.slice(0, -1)
+    }
 }
 
 function geraLink(){
     if (telefone.length == 11){
         divLink.style.borderWidth = "2px";
+        txt.style.color = "black"
         divLink.innerText = `https://wa.me//55${telefone}`
     } else {
         alert("Telefone Invalido")
